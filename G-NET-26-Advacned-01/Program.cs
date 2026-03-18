91a277d694b0c8de2afe5af1c0022d22691deffe
Helmy-Fathy <7elmyfathy@gmail.com>
2026-03-18 19:26:57 +0200

    Q1

--- G-NET-26-Advacned-01/Program.cs
+++ G-NET-26-Advacned-01/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Q1
+            //Q1: What is a Generic Class? Why Use Generics?
+            /*
+             A generic class is a class that uses a type parameter (e.g., T) as a placeholder for a data type. 
+             The actual type is specified when an instance is created , making one class work for any type.
+
+            Why Use Generics?
+            -	Type Safety : Errors are caught at compile time, not runtime.
+            -	Performance : No boxing/unboxing for value types (unlike object).
+            -	Code Reuse : Write one class/method for all types instead of duplicating code.
+            -	Better IntelliSense : IDE knows the real type, giving better auto-complete.
+             */
+            #endregion
+
+            #region Q2
+            //
+            #endregion
         }
     }
 }
